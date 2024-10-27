4639bfa5bbfece5095afef3ca9545927d8dfc9c6
Martseniuk Alina <martseniuk.av@gmail.com>
2024-10-27 20:22:57 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+let result = 5 + 5 + '5';
+const email = "martseniuk.av@gmail.com";
+const My = "My";
+const name = "name";
+const is = "is";
+const fullName = My + name + is;
+const fulName = fullName + 'Viktor';
+const userName = "Олександр";
+const payment = 300;   
+const message = "Дякуємо, Олександро! До сплати 300 гривень";
+alert(message);
+
+console.log(result);
+console.log(typeof result); 
+console.log(email.includes("@"));
+console.log(email.length);
+console.log(fulName);
+
